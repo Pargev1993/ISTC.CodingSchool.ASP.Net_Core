d02651906f6c268996e591cffdb05cce99011ed7
pqoalla <pqoalla@gmail.com>
2019-03-19 06:25:52 -0700

    ASP.Netc Core Web Api Monipulation

---
+++ ASP.Net_CoreApp/ASP.Net-Core-WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.Net_Core_WebApi.Controllers
+{
+    
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        [Route("[controller]/test1")]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        [Route("api/test2")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        [Route("api/test3")]
+        public void Post([FromBody] string value)
+        {
+            value = "https://github.com/Pargev1993";
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        [Route("api/test4")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
